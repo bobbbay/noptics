26c3ee24953dffc19e62939924a7633eb5309cd0
Bobbbay <abatterysingle@gmail.com>
2023-06-24 17:06:31 +0000

    feat: initial commit

---
+++ examples/robot.rs
@@ -0,0 +1,64 @@
+use futures::StreamExt;
+use noptics::{Data, Lens};
+use tokio::pin;
+
+#[derive(Default, Copy, Clone)]
+struct GyroRotDeg(usize);
+
+#[derive(Default)]
+struct InputState {
+    gyro_rot_deg: GyroRotDeg,
+}
+
+// START auto-generated
+
+#[derive(Clone)]
+struct InputState1 {
+    gyro_rot_deg: Lens<Self, GyroRotDeg>,
+}
+
+impl Default for InputState1 {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            gyro_rot_deg: Lens::new(Default::default()),
+        }
+    }
+}
+
+impl Data for GyroRotDeg {}
+
+// END auto-generated
+
+#[tokio::main]
+async fn main() {
+    let input_state = InputState1::default();
+
+    let my_lens = input_state.gyro_rot_deg;
+    let my_lens_2 = my_lens.clone();
+
+    tokio::spawn(async move {
+        let s = my_lens.stream();
+        pin!(s);
+
+        while let Some(value) = s.next().await {
+            let value = value.0;
+            println!("{value}");
+        }
+    });
+
+    tokio::spawn(async move {
+        let mut i = 0;
+
+        loop {
+            if i > 10 {
+                return;
+            }
+            my_lens_2.set(GyroRotDeg(i));
+            i += 1;
+            tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+        }
+    });
+
+    loop {}
+}
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::{marker::PhantomData, sync::Arc};
+
+use atomic::{Atomic, Ordering};
+use futures::Stream;
+use tokio::sync::watch::{self, Receiver, Sender};
+
+/// Marker trait for all kinds of Tarsier Data. Data is a piece of state
+/// that is Copy, small, and efficient.
+pub trait Data: Copy {}
+
+/// A Lens is a functional method to access one part of a larger data
+/// structure, as long as the owner and its data implement [`Data`]. Lenses
+/// can interact independently, store infromation separately, and using them
+/// is always safe.
+///
+/// Cloning a Lens will return a reference to the same data. Modifying a
+/// cloned Lens will modify all Lenses cloned from it.
+#[derive(Clone)]
+pub struct Lens<O, E: Data> {
+    element: Arc<Atomic<E>>,
+    tx: Arc<Sender<()>>,
+    rx: Receiver<()>,
+    /// We store `O` for ease of use in identifying which Lens comes from
+    /// which data. The owner type is not relevant to the implementation.
+    _ph: PhantomData<O>,
+}
+
+impl<O, E: Data> Lens<O, E> {
+    /// Construct a new Lens containing some value of type E.
+    pub fn new(element: E) -> Self {
+        let (tx, rx) = watch::channel(());
+
+        Self {
+            element: Arc::new(Atomic::new(element)),
+            tx: Arc::new(tx),
+            rx,
+            _ph: PhantomData,
+        }
+    }
+
+    /// Get an immutable reference to the data.
+    pub fn get(&self) -> E {
+        self.element.load(Ordering::Relaxed)
+    }
+
+    pub fn set(&self, element: E) {
+        self.element.store(element, Ordering::Relaxed);
+        self.tx.send(()).unwrap();
+    }
+
+    pub fn stream(&self) -> impl Stream<Item = E> + '_ {
+        // Clone an instance of `rx` to use in this stream.
+        let mut rx = self.rx.clone();
+
+        async_stream::stream! {
+            loop {
+                rx.changed().await.unwrap();
+                yield self.get();
+            }
+        }
+    }
+}
